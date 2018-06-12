625b7bc585f0bf1cb8e4e4e71583d7791afe9405
vladimirwest <west957@mail.ru>
2018-06-13 01:47:22 +0300

    initial commit

---
+++ voicebot.py
@@ -0,0 +1,54 @@
+﻿import time
+import os
+import json
+import subprocess
+import urllib.request
+import vk_api
+import requests
+from wit import Wit
+from contextlib import closing
+
+token='336b280042d94d9076bccbf63de2c22c8cd07bd2b5173bd7e2e22ff55e38be3e69e19bac0b32745765735'
+wit_token = 'RUFRTLTN4IILNBFUPMOHRUJMEPXFZYKO'
+vk = vk_api.VkApi(token = token, api_version='5.73') # Токен Vk Api
+values = {'out' : 0, 'count' : 100, 'time_offset' : 60} # Данные для обновления информации о входящих сообщениях
+
+
+
+def recognize_voice(data, link, token, wit_token):
+    user_id = data['user_id']
+    client = Wit(wit_token)
+    doc = requests.get(link)
+    resp = None
+    with closing(doc):
+        try:
+            resp = client.speech(doc.content, None, {'Content-Type': 'audio/mpeg3'})
+            resp = str(resp['_text'])
+        except:
+            resp = "Не удалось распознать сообщение"
+        finally:
+            vk.method('messages.send', {'user_id': user_id, 'message': resp})
+    return
+
+
+def main():
+	while True: # Бесконечный цикл
+		time.sleep(1) # Задержка
+		response = vk.method('messages.get', values) # Получаем список сообщений
+        # Смотрим, какием сообщения новые
+		if response['items']:
+			values['last_message_id'] = response['items'][0]['id']
+        #Отвечаем
+		for item in response['items']:
+			data = item
+			while(data.get('fwd_messages',"")!=""):
+				data = data.get('fwd_messages')
+				data=dict(data[0])
+			if(data.get('attachments',"")!=""):
+				a = data['attachments']
+				if a[0]['type'] == 'doc':
+					if a[0]['doc']['type'] == 5:
+						recognize_voice(item, a[0]['doc']['preview']['audio_msg']['link_mp3'], token, wit_token)
+						
+if __name__ == '__main__':
+    main()
